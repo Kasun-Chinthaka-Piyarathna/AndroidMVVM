cc09c0d6416c63e028f927966529f15ed5eb1dcc
Kasun Chinthaka <kasun@odoc.life>
2021-08-30 12:25:06 +0530

    commit changes-Android MVVM sample

---
+++ app/src/main/java/com/example/androidmvvm/viewmodels/AppViewModel.java
@@ -0,0 +1,66 @@
+package com.example.androidmvvm.viewmodels;
+
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import com.example.androidmvvm.BR;
+import com.example.androidmvvm.models.Model;
+
+public class AppViewModel extends BaseObservable {
+
+    private Model model;
+    private String successMessage = "Login successful";
+    private String errorMessage = "Email or Password is not valid";
+
+    @Bindable
+    private String toastMessage = null;
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+    private void setToastMessage(String toastMessage) {
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    @Bindable
+    public String getUserEmail() {
+        return model.getEmail();
+    }
+
+    public void setUserEmail(String email) {
+        model.setEmail(email);
+        notifyPropertyChanged(BR.userEmail);
+    }
+
+    @Bindable
+    public String getUserPassword() {
+        return model.getPassword();
+    }
+
+    public void setUserPassword(String password) {
+        model.setPassword(password);
+        notifyPropertyChanged(BR.userPassword);
+    }
+
+    public AppViewModel() {
+        model = new Model("", "");
+    }
+
+    public void onButtonClicked() {
+        if (isValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+
+    public boolean isValid() {
+        return !TextUtils.isEmpty(getUserEmail()) && Patterns.EMAIL_ADDRESS.matcher(getUserEmail()).matches()
+                && getUserPassword().length() > 5;
+    }
+}
+
